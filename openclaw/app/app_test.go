b755a627eb0d9dbca9bf0b772f1014ec08844c35
winechord <guoqizhou19@gmail.com>
2026-07-09 17:07:03 +0800

    openclaw/app: finalize near request deadline

--- openclaw/app/app_test.go
+++ openclaw/app/app_test.go
@@ -4307,6 +4307,14 @@ func TestValidateAgentRunOptions(t *testing.T) {
 			wantErr: true,
 		},
 		{
+			name:      "deadline-finalization-window",
+			agentType: agentTypeClaudeCode,
+			opts: runOptions{
+				DeadlineFinalizationWindow: time.Minute,
+			},
+			wantErr: true,
+		},
+		{
 			name:      "preload-memory",
 			agentType: agentTypeClaudeCode,
 			opts: runOptions{
